633859e75574872baa05c722f0319dc26dafd161
NanJiang327 <nj499521010@gmail.com>
2019-04-21 16:32:05 +1200

    Add server files and update gitignore

---
+++ server/model.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+// Connect mongodb
+const DB_URL = 'mongodb://localhost:27017/movie-in-one'
+
+const models = {
+  user: {
+    'email': {type: String, require: true},
+    'username': {type: String, require: true},
+    'password': {type: String, require: true},
+    'language': {type: String, require: true},
+    'avatar': {type: String, require: false}
+  },
+  comment: {
+    'userId': {type: String, require: true},
+    'author': {type: String, require: true},
+    'content': {type: String, require: true},
+    'movieId': {type: String, require: true}
+  }
+    
+}
+
+for (let m in models) {
+  mongoose.model(m, new mongoose.Schema(models[m]))
+}
+
+// User.create({
+//   email: 'aaronj.9403@gmail.com',
+//   password: '123.comwyxx'
+// }, function(err, doc) {
+//   if (!err) {
+//     console.log(doc)
+//   } else {
+//     console.log(err)
+//   }
+// })
+
+mongoose.connect(DB_URL, { useNewUrlParser: true });
+mongoose.connection.on('connected', function(){
+  console.log('mongo connect success')
+})
+
+module.exports = {
+  getModel: function(name) {
+    return mongoose.model(name)
+  }
+}
