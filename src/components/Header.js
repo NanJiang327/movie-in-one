2769d1776beed61ce2e81284b896c5e8fefd75ef
NanJiang327 <nj499521010@gmail.com>
2019-04-22 16:35:24 +1200

    Add logout button after logged in

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,34 @@ import Search from './Search'
 import { Link } from 'react-router-dom'
 import { Menu, Dropdown, Icon } from 'antd';
 import { connect } from 'react-redux';
-import { changeLang } from '../store/action'
+import { changeLang, logout, loadData } from '../store/action'
+import browserCookies from 'browser-cookies'
+import axios from 'axios'
 
 class Header extends Component {
 
   onClick = ({ key }) => {
     this.props.changeLang(key)
+    if (this.props.user.logged) {
+      console.log(key)
+      axios.post('/user/changeLang', {username: this.props.user.username, language: key})
+        .then(res => {
+          console.log(this.props.language)
+        })
+        .catch(err =>{
+          console.log(err)
+        })
+    }
   };
+
+  componentDidMount () {
+    this.props.loadData()
+  }
+
+  clickLogout () {
+    browserCookies.erase('connect.sid')
+    this.props.logout()
+  }
 
   render () {
     const menu = (
@@ -32,16 +53,31 @@ class Header extends Component {
       <div className="header">
         <div className="menu">
           <div className="menu-left">
-            <Link to='/'>
+            <Link to='/' replace>
               <IconFont name={'movie'} />
               Movie in One
             </Link>
           </div>
           <div className="menu-right">
-            <Link to='/login' className="login-btn">
-              <IconFont name={'acc'} className='movie-title'/>
-              Login
-            </Link>
+            {
+              this.props.user.logged ? 
+                <button 
+                  onClick={() => {this.clickLogout()}}
+                  className="login-btn" 
+                >
+                  <IconFont name={'acc'} className='movie-title'/>
+                  Logout
+                </button> :
+                <Link 
+                  to='/login' 
+                  className="login-btn" 
+                  replace
+                
+                >
+                  <IconFont name={'acc'} className='movie-title'/>
+                  Login
+                </Link>
+            }
             <Dropdown overlay={menu} className="dropdown-menu">
               <div className="ant-dropdown-link" >
                 <IconFont name={'lang'} className='movie-title'/>
@@ -63,13 +99,16 @@ class Header extends Component {
 
 function mapStateToProps(state) {
   return {
-    language: state.language
+    language: state.language,
+    user: state.user
   }
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-    changeLang: (language) => dispatch(changeLang(language))
+    changeLang: (language) => dispatch(changeLang(language)),
+    logout: () => dispatch(logout()),
+    loadData:() => dispatch(loadData())
   };
 }
 
