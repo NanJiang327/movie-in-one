0e3907003ac744c21284540eb52e5fd7c285aeef
AaronJ <aaron.jiang@9spokes.com>
2021-03-12 20:55:09 +1300

    Change redirect url

--- src/pages/Remove/index.js
+++ src/pages/Remove/index.js
@@ -8,7 +8,7 @@ class Remove extends Component {
       <div className='remove-page'>
         <h1>
           <IconFont name={'404'} className=''/>
-          Oops! This website has been moved to <a href="http://movie-in-one.info" >http://movie-in-one.info</a>
+          Oops! This website has been moved to <a href="https://stark-basin-71839.herokuapp.com/" >http://movie-in-one.info</a>
         </h1>
         <div>
           <a className="button" href="http://movie-in-one.info" >Go to new Movie in One</a>
